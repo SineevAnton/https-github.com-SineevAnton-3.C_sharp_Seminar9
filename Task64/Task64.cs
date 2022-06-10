4a63845435c2a06e8e8665382bd0c038a41ce611
Anton Sineev <sineevanton@mail.ru>
2022-06-10 13:11:44 +0300

    Task64 complete

---
+++ Task64/Task64.cs
@@ -0,0 +1,21 @@
+﻿// Set the value to N. Write a program that outputs all
+// natural numbers in the range from N to 1. Execute using recursion.
+
+Console.Write("Enter the value of N: ");
+int num = int.Parse(Console.ReadLine());
+
+void ShowNumbersFromNToOne(int n)
+{
+    if (n == 1)
+    {
+        Console.Write($"{n} ");
+        return;
+    }
+    else
+    {
+        Console.Write($"{n} ");
+        ShowNumbersFromNToOne(n - 1);
+    }
+}
+
+ShowNumbersFromNToOne(num);
