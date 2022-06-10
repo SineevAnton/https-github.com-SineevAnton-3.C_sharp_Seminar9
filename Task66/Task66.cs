6278ee13c2ad2a4e72d0f945bc6b4e34ae7dd980
Anton Sineev <sineevanton@mail.ru>
2022-06-10 13:36:40 +0300

    Task66 complete

---
+++ Task66/Task66.cs
@@ -0,0 +1,28 @@
+﻿// Set the values of M and N. Write a program that finds the sum
+// of natural elements in the range from M to N. Execute using recursion.
+
+Console.Write("Enter the value of N: ");
+int numN = int.Parse(Console.ReadLine());
+Console.Write("Enter the value of M: ");
+int numM = int.Parse(Console.ReadLine());
+
+int GetSumFromMToN(int m, int n)
+{
+    if (m > n)
+    {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+    //int sum = 0;
+    if (n == m)
+    {
+        return n;
+    }
+    else
+    {
+        return n + GetSumFromMToN(m, n - 1);
+    }
+}
+
+Console.WriteLine(GetSumFromMToN(numM, numN));
