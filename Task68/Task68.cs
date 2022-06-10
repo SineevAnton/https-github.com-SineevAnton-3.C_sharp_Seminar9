82559ce616d7cacc5dfd7dfd17d2ce49d91207a7
Anton Sineev <sineevanton@mail.ru>
2022-06-10 13:55:58 +0300

    Task68 complete

---
+++ Task68/Task68.cs
@@ -0,0 +1,30 @@
+﻿// Write a program for calculating the Ackerman function using
+// recursion. Two non-negative numbers m and n are given.
+// Learn more about the Ackerman function: https://progler.ru/blog/funkciya-akkermana
+
+Console.Write("Enter the value of M: ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Enter the value of N: ");
+int numN = int.Parse(Console.ReadLine());
+
+int Ackerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+    {
+        return Ackerman(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0))
+    {
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+    }
+    else
+    {
+        return n + 1;
+    }
+}
+
+Console.WriteLine(Ackerman(numM, numN));
